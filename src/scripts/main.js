11e0b49abf69418a2698c8f06829a97aee2c52b7
annbisha <annskaylove@gmail.com>
2026-04-21 12:15:26 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+'use strict';
+
+const inputs = document.querySelectorAll('.field input');
+
+inputs.forEach((input) => {
+  const inputName = input.name;
+
+  const labelText = inputName[0].toUpperCase() + inputName.slice(1);
+
+  input.placeholder = labelText;
+
+  const label = document.createElement('label');
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = labelText;
+
+  input.parentElement.appendChild(label);
+});
